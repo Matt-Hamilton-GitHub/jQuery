d98df9f41b77783c36e35bb8f1ad14a863599e44
Matt <i.w.b.s.bilm@gmail.com>
2021-07-01 12:07:37 -0700

    project in progress

---
+++ jQuery.js
@@ -0,0 +1,50 @@
+
+
+$('#btn').click(function(){
+  
+    $.ajax({
+        method: 'GET',
+        url: '//',
+        dataType: 'json'
+       })
+       .done(()=>{
+
+        })
+        .fail(()=>{
+
+        })
+
+
+})
+
+$("#getBtn").click(function(){
+    $.get('https://api.github.com/users/colt')
+    .done(function(data){
+      console.log(data);
+    })
+    .fail(function(){
+      console.log("ERROR!");
+    })
+  });
+  
+  $("#postBtn").click(function(){
+   var data = {name: "Charlie", city: "Florence"};
+   $.post("www.catsarecoolandsoaredogs.com", data)
+    .done(function(data){
+     console.log("HI!");
+   })
+    .fail(function(){
+     console.log("ERROR!");
+   })
+  });
+  
+  $("#getJSONBtn").click(function(){
+    $.getJSON("https://api.github.com/users/colt")
+    .done(function(data){
+      console.log(data);
+    })
+    .fail(function(){
+      console.log("PROBLEM!");
+    })
+  });
+  
