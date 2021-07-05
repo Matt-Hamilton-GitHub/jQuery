a97fef87bdec33ca35b1b2363afb9cbb0e10fa41
Matt <i.w.b.s.bilm@gmail.com>
2021-07-04 21:53:53 -0700

    not suficient commit

--- jQuery.js
+++ jQuery.js
@@ -1,50 +1,74 @@
 
+var setCat = document.querySelector('#cat-img');
 
-$('#btn').click(function(){
-  
-    $.ajax({
-        method: 'GET',
-        url: '//',
-        dataType: 'json'
-       })
-       .done(()=>{
 
-        })
-        .fail(()=>{
+$('#get-cat-btn').click(()=>{
 
-        })
+$.getJSON('http://aws.random.cat/meow')
+
+.done((data)=>{
+ setCat.src = data.file;
+ console.log(data);
+}).fail((data)=>{
+
+  console.log('failed to render images');
+})
+
 
 
 })
 
-$("#getBtn").click(function(){
-    $.get('https://api.github.com/users/colt')
-    .done(function(data){
-      console.log(data);
-    })
-    .fail(function(){
-      console.log("ERROR!");
-    })
-  });
+
+
+
+// $('#btn').click(function(){
   
-  $("#postBtn").click(function(){
-   var data = {name: "Charlie", city: "Florence"};
-   $.post("www.catsarecoolandsoaredogs.com", data)
-    .done(function(data){
-     console.log("HI!");
-   })
-    .fail(function(){
-     console.log("ERROR!");
-   })
-  });
+//     $.ajax({
+//         method: 'GET',
+//         url: '//',
+//         dataType: 'json'
+//        })
+//        .done(()=>{
+
+//         })
+//         .fail(()=>{
+
+//         })
+
+
+// })
+
+// $("#getBtn").click(function(){
+//     $.get('https://api.github.com/users/colt')
+//     .done(function(data){
+//       console.log(data);
+//     })
+//     .fail(function(){
+//       console.log("ERROR!");
+//     })
+//   });
   
-  $("#getJSONBtn").click(function(){
-    $.getJSON("https://api.github.com/users/colt")
-    .done(function(data){
-      console.log(data);
-    })
-    .fail(function(){
-      console.log("PROBLEM!");
-    })
-  });
+//   $("#postBtn").click(function(){
+//    var data = {name: "Charlie", city: "Florence"};
+//    $.post("www.catsarecoolandsoaredogs.com", data)
+//     .done(function(data){
+//      console.log("HI!");
+//    })
+//     .fail(function(){
+//      console.log("ERROR!");
+//    })
+//   });
   
+//   $("#getJSONBtn").click(function(){
+//     $.getJSON("https://api.github.com/users/colt")
+//     .done(function(data){
+//       console.log(data);
+//     })
+//     .fail(function(){
+//       console.log("PROBLEM!");
+//     })
+//   });
+  
+
+
+
